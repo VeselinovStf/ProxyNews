aba9e5b0b033b8b9667345d5e586519d7d52bef4
VeselinovStf <veselinovStf@gmail.com>
2019-04-29 09:12:47 +0300

    Editing validation exception -> to send exception to logger

--- src/RssReader.ModelFactory.Implementation/RssFeed/RssFeedModeFactory.cs
+++ src/RssReader.ModelFactory.Implementation/RssFeed/RssFeedModeFactory.cs
@@ -6,22 +6,37 @@ using System.Linq;
 using Utility.RssReading.RssReader.ModelFactory.Abstract;
 using Utility.RssReading.RssReader.Models.Implementations;
 using RssReader.ModelFactory.Validator.Abstract;
+using Microsoft.Extensions.Logging;
 
 namespace Utility.RssReading.RssReader.ModelFactory.Implementation.RssFeed
 {
     public class RssFeedModeFactory : IRssReaderModelFactory<BaseRssFeed>
     {
         private readonly IModelFactoryValidator<BaseRssFeed> modelFactoryValidator;
+        private readonly ILogger<RssFeedModeFactory> logger;
 
-        public RssFeedModeFactory(IModelFactoryValidator<BaseRssFeed> modelFactoryValidator)
+        public RssFeedModeFactory(
+            IModelFactoryValidator<BaseRssFeed> modelFactoryValidator,
+            ILogger<RssFeedModeFactory> logger)
         {
             this.modelFactoryValidator = modelFactoryValidator;
+            this.logger = logger;
         }
 
         public IEnumerable<BaseRssFeed> Create(IEnumerable<XElement> elements)
         {
-            var RSSFeedData = (from x in elements
-                               select this.modelFactoryValidator.ValidateRssFeedModel(x));
+            IList<BaseRssFeed> RSSFeedData = new List<BaseRssFeed>();
+            foreach (var e in elements)
+            {
+                try
+                {
+                    RSSFeedData.Add(this.modelFactoryValidator.ValidateRssFeedModel(e));
+                }
+                catch (Exception ex)
+                {
+                    this.logger.LogWarning(ex.Message);
+                }
+            }
 
             return RSSFeedData;
         }
