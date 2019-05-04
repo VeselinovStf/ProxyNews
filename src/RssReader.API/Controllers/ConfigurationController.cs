b6d645035a730a0ecab483c4dbca717b8491d6c3
VeselinovStf <veselinovStf@gmail.com>
2019-05-04 20:05:49 +0300

    Creating RssReader.API

---
+++ src/RssReader.API/Controllers/ConfigurationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RssReader.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        public IActionResult ViewAll()
+        {
+            return Ok("value all");
+        }
+
+        public IActionResult Get(string confName)
+        {
+            return Ok("value single");
+        }
+    }
+}
---
+++ src/RssReader.API/Controllers/ModelConfigurationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RssReader.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelConfigurationController : ControllerBase
+    {
+        public IActionResult Update(string propName)
+        {
+            return Ok("change prop");
+        }
+
+        public IActionResult Insert(string propName)
+        {
+            return Ok("add prop");
+        }
+
+        public IActionResult Remove(string propName)
+        {
+            return Ok("add prop");
+        }
+    }
+}
